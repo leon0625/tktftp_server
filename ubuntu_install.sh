a848a9e0f3bd4a2c908f7206651aa07211c232cb
leon0625 <liangming.liu@foxmail.com>
2024-06-03 09:46:47 +0800

    添加安装文件

---
+++ ubuntu_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+INSTALL_DIR=/APP/tktftp
+sudo ls
+pyinstaller -F tktftp.py --add-data 'icon.png:.' --hidden-import=PIL._tkinter_finder
+sudo mkdir -p /APP/tktftp
+sudo cp dist/tktftp $INSTALL_DIR/
+sudo cp icon.png $INSTALL_DIR/
+sudo cp tktftp.desktop /usr/local/share/applications/
+sudo cp tktftp.policy /usr/share/polkit-1/actions/
+ 
