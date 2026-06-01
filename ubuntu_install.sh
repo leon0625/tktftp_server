4391bec9bf795218d3c7eca894299f130810c77f
leon0625 <liangming.liu@foxmail.com>
2026-06-01 20:12:13 +0800

    更新图标

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -6,6 +6,6 @@ INSTALL_DIR=/APP/wails_tftp
 sudo mkdir -p $INSTALL_DIR/
 sudo cp wails_tftp/build/bin/wails_tftp $INSTALL_DIR/
 sudo sudo setcap cap_net_bind_service=+ep $INSTALL_DIR/wails_tftp
-sudo cp icon.png $INSTALL_DIR/
+sudo cp wails_tftp/appicon.png $INSTALL_DIR/icon.png
 sudo cp wails_tftp.desktop /usr/local/share/applications/
 echo finish
